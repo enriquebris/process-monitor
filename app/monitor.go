636d590db915072bb8190d19290510df6147e549
Enrique Bris <cubanhenry5@gmail.com>
2022-05-05 17:53:24 -0400

    checking subprocesses too

--- app/monitor.go
+++ app/monitor.go
@@ -4,6 +4,7 @@ import (
 	"github.com/robfig/cron/v3"
 	"log"
 	"regexp"
+	"sync"
 )
 
 // *****************************************************************************
@@ -11,12 +12,22 @@ import (
 // *****************************************************************************
 
 type MonitorEntry struct {
-	NameRegex               string `json:"name_regex"`
+	// process(es) to monitor
+	NameRegex string `json:"name_regex"`
+	// how often to check
+	Cron string `json:"cron"`
+	// CPU max limit
+	CPUMaxLimit float64 `json:"cpu_max_limit"`
+	// whether to kill process(es) if CPU limit is reached (after TotalAttemptsBeforeKill attempts)
+	KillIfCPUMaxLimit bool `json:"kill_if_cpu_max_limit"`
+	// whether to kill the parent process if the sum of children attempts exceeds the TotalAttemptsBeforeKill limit
+	KillIfChildrenCPUMaxLimit bool `json:"kill_if_children_cpu_max_limit"`
+	// number of attempts to check before killing process(es) (only if KillIfCPUMaxLimit is true)
+	TotalAttemptsBeforeKill int `json:"total_attempts_before_kill"`
+
+	// processes keep track of processes matching NameRegex: [pid]total consecutive times exceeding CPUMaxLimit
+	processes               sync.Map
 	regex                   *regexp.Regexp
-	Cron                    string  `json:"cron"`
-	CPUMaxLimit             float64 `json:"cpu_max_limit"`
-	KillIfCPUMaxLimit       bool    `json:"kill_if_cpu_max_limit"`
-	TotalAttemptsBeforeKill uint    `json:"total_attempts_before_kill"`
 	totalAttemptsBeforeKill uint
 }
 
@@ -29,19 +40,71 @@ func (st *MonitorEntry) GetCronFunction() func() {
 	}
 }
 
+// resetAttemptsForPID resets the number of consecutive times a process has exceeded CPUMaxLimit (for a given PID)
+func (st *MonitorEntry) resetAttemptsForPID(pid int) {
+	st.processes.Store(pid, 0)
+}
+
+// removeAttemptsForAllPIDsNotInTheMap removes all processes having PIDs not in the given map
+func (st *MonitorEntry) removeAttemptsForAllPIDsNotInTheMap(mp map[int]struct{}) {
+	st.processes.Range(func(key, value interface{}) bool {
+		if _, ok := mp[key.(int)]; !ok {
+			st.processes.Delete(key)
+		}
+		return true
+	})
+}
+
+// deprecated
 // resetAttempts resets the attempts counter
 func (st *MonitorEntry) resetAttempts() {
 	st.totalAttemptsBeforeKill = 0
 }
 
+// incrementAttemptsForPID increments the total attempts counter for a given PID
+func (st *MonitorEntry) incrementAttemptsForPID(pid int) {
+	var actual int
+
+	if actualRaw, ok := st.processes.Load(pid); ok {
+		actual = actualRaw.(int)
+	}
+
+	st.processes.Store(pid, actual+1)
+}
+
+// deprecated
 func (st *MonitorEntry) incrementAttempts() {
 	st.totalAttemptsBeforeKill++
 }
 
+// getAttemptsForPID returns the number of consecutive times a process has exceeded CPUMaxLimit (for a given PID)
+func (st *MonitorEntry) getAttemptsForPID(pid int) int {
+	actualRaw, ok := st.processes.Load(pid)
+	if !ok {
+		return 0
+	}
+	return actualRaw.(int)
+}
+
+// getAllAttempts returns the number of consecutive times all processes have exceeded CPUMaxLimit
+func (st *MonitorEntry) getAllAttempts() int {
+	total := 0
+
+	st.processes.Range(func(key, value interface{}) bool {
+		total += value.(int)
+
+		return true
+	})
+
+	return total
+}
+
+// deprecated
 func (st *MonitorEntry) getAttempts() uint {
 	return st.totalAttemptsBeforeKill
 }
 
+// getRegex returns the regex compiled from NameRegex
 func (st *MonitorEntry) getRegex() *regexp.Regexp {
 	if st.regex == nil {
 		st.regex = regexp.MustCompile(st.NameRegex)
